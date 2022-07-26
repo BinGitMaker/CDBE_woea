9b6ffab472a5e1d682d346741a34e99176f74b2b
BinGitMaker <dbinoisdevpro@gmail.com>
2022-07-26 13:53:55 +0200

    Security et authentification connexion

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -5,45 +5,76 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname', TextType::class,[
-                'label' => 'Votre prénom :',
-                'attr' => [
-                    'placeholder' => 'Saisissez votre prénom'
+        ->add('firstname', TextType::class,[
+            'label' => 'Votre prénom :',
+            'constraints' => new Length(
+                [
+                    'min' => 2,
+                    'max' => 30,
+                    'minMessage' => 'Votre prénom est trop court, il doit comporter {{ limit }} caractères au minimum',
+                    'maxMessage' => 'Votre prénom est trop long, il doit comporter {{ limit }} caractères au maximum',
                 ]
-            ])
-            ->add('lastname', TextType::class,[
-                'label' => 'Votre nom :',
-                'attr' => [
-                    'placeholder' => 'Saisissez votre nom'
+            ), 
+            'attr' => [
+                'placeholder' => 'Saisissez votre prénom',
+            ]
+        ])
+        ->add('lastname', TextType::class,[
+            'label' => 'Votre nom :',
+            'constraints' => new Length(
+                [
+                    'min' => 2,
+                    'max' => 30,
+                    'minMessage' => 'Votre nom est trop court, il doit comporter {{ limit }} caractères au minimum',
+                    'maxMessage' => 'Votre nom est trop long, il doit comporter {{ limit }} caractères au maximum',
                 ]
-            ])
-            ->add('email', EmailType::class,[
-                'label' => 'Votre mail :',
-                'attr' => [
-                    'placeholder' => 'Saisissez votre mail'
-                ]
-            ])
-            ->add('password', PasswordType::class,[
-                'label' => 'Votre mot de passe :',
+            ),
+            'attr' => [
+                'placeholder' => 'Saisissez votre nom'
+            ]
+        ])
+        ->add('email', EmailType::class, [
+            'label' => 'Votre email :',
+            'attr' => [
+                'placeholder' => 'Saisissez votre email'
+            ]
+        ])
+        ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class, 
+            'invalid_message' => 'Le mot de passe et la confirmation doivent être identiques!',
+            'label' => 'Votre mot de passe :',
+            'required' => true,
+            'first_options' => [
+                'label' => 'Mot de passe',
                 'attr' => [
                     'placeholder' => 'Saisissez votre mot de passe'
                 ]
-            ])
-            ->add('submit', SubmitType::class,[
-                'label' => 'S\'inscrire',
-            ])
-        ;
+            ],
+            'second_options' => [
+                'label' => 'Confirmation du mot de passe',
+                'attr' => [
+                    'placeholder' => 'Merci de confirmer votre mot de passe'
+                ]
+            ],
+            
+        ])
+        ->add('submit', SubmitType::class, [
+            'label' => 'S\'inscrire',
+        ])
+    ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
