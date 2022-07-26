51295eeb37bdf0ff46a39f4cf345c41c878fcb1a
BinGitMaker <dbinoisdevpro@gmail.com>
2022-07-26 11:46:13 +0200

    ajout des champs à User et mise en forme de RegisterType

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,15 +6,42 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            
-            ->add('password')
+            ->add('firstname', TextType::class,[
+                'label' => 'Votre prénom :',
+                'attr' => [
+                    'placeholder' => 'Saisissez votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class,[
+                'label' => 'Votre nom :',
+                'attr' => [
+                    'placeholder' => 'Saisissez votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'Votre mail :',
+                'attr' => [
+                    'placeholder' => 'Saisissez votre mail'
+                ]
+            ])
+            ->add('password', PasswordType::class,[
+                'label' => 'Votre mot de passe :',
+                'attr' => [
+                    'placeholder' => 'Saisissez votre mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class,[
+                'label' => 'S\'inscrire',
+            ])
         ;
     }
 
